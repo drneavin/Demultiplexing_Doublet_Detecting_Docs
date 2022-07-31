6f03001c9b0243ca070b6449c5c4bb58ae3545fb
drneavin <d.neavin@garvan.org.au>
2022-07-31 17:14:11 +1000

    major update to doubletdetection function

--- scripts/DoubletDetection.py
+++ scripts/DoubletDetection.py
@@ -9,9 +9,18 @@ parser.add_argument("-m", "--counts_matrix", required = True, help = "cell range
 parser.add_argument("-b", "--barcodes", required = False, default = None, help = "File containing droplet barcodes. Use barcodes from provided 10x dir by default.")
 parser.add_argument("-f", "--filtered_barcodes", required = False, default = None, help = "File containing a filtered list of droplet barcodes. This may be used if you want to use a filtered list of barcodes for doublet detection (ie need to remove droplets that are empty or high in ambient RNA).")
 parser.add_argument("-o", "--outdir", required = False, default = os.getcwd(), help = "The output directory; default is current working directory")
+parser.add_argument("-p", "--boost_rate", required = False, default = 0.25, type = float, help = "Proportion of cells used to generate synthetic doublets; default is 0.25.")
+parser.add_argument("-c", "--n_components", required = False, default = 30, type = int, help = "Number of principal components to use; default is 30.")
+parser.add_argument("-g", "--n_top_var_genes", required = False, default = 1000, type = int, help = "Number of top variable genes to use; default is 1000.")
+parser.add_argument("-r", "--replace", required = False, default = False, help = "Whether to replace cells when generating synthetic doublets; default is False.")
+parser.add_argument("-a", "--clustering_algorithm", required = False, default = 'phenograph', help = "Which clustering algorithm to use; default is 'phenograph'")
+parser.add_argument("-k", "--clustering_kwargs", required = False, default = None, help = "Keyword arguments to pass to clustering algorithm; default is None.")
 parser.add_argument("-i", "--n_iterations", required = False, default = 50, type = int, help = "Number of iterations to use; default is 50")
-parser.add_argument("-p", "--phenograph", required = False, default = False, help = "Whether to use phenograph (True) or not (False); default is False")
+parser.add_argument("-e", "--pseudocount", required = False, default = 0.1, type = float, help = "Pseudocount used to normalize counts; default is 0.1.")
+parser.add_argument("-n", "--normalizer", required = False, default = None, help = "Method for raw counts normalization; default is None.")
+parser.add_argument("-d", "--random_state", required = False, default = 0, type = int, help = "Number to use to seed random state for PCA; default is 0.")
 parser.add_argument("-s", "--standard_scaling", required = False, default = True, help = "Whether to use standard scaling of normalized count matrix prior to PCA (True) or not (False); default is True")
+parser.add_argument("-j", "--n_jobs", required = False, default = 1, type = int, help = "Number of jobs to to use; default is 1")
 parser.add_argument("-t", "--p_thresh", required = False, default = 1e-16, type = float, help = "P-value threshold for doublet calling; default is 1e-16")
 parser.add_argument("-v", "--voter_thresh", required = False, default = 0.5, type = float, help = "Voter threshold for doublet calling; default is 0.5")
 args = parser.parse_args()
@@ -34,12 +43,12 @@ import read10x
 
 
 
-if args.phenograph == 'True':
+if args.clustering_kwargs == 'True':
     pheno = True
-elif args.phenograph == 'False':
+elif args.clustering_kwargs == 'False':
     pheno = False
 else:
-    pheno = args.phenograph
+    pheno = args.clustering_kwargs
 
 
 if args.standard_scaling == 'True':
@@ -102,7 +111,7 @@ else:
         exit()
 
 
-clf = doubletdetection.BoostClassifier(n_iters=args.n_iterations, use_phenograph=pheno, standard_scaling=standard_scaling, verbose = True)
+clf = doubletdetection.BoostClassifier(boost_rate=args.boost_rate, n_components=args.n_components, n_top_var_genes=args.n_top_var_genes, replace=args.replace, clustering_algorithm=args.clustering_algorithm, clustering_kwargs=args.clustering_kwargs, n_iters=args.n_iterations, normalizer=args.normalizer, pseudocount=args.pseudocount, random_state=args.random_state, standard_scaling=args.standard_scaling, n_jobs=args.n_jobs, verbose = True)
 doublets = clf.fit(raw_counts).predict(p_thresh=args.p_thresh, voter_thresh=args.voter_thresh)
 
 results = pd.Series(doublets, name="DoubletDetection_DropletType")
