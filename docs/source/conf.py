d75372d5448c76e6e14683254018a79ac3eb38eb
drneavin <d.neavin@garvan.org.au>
2023-01-20 18:23:30 +1100

    updated to v2.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2021, Garvan Institute of Medical Research'
 author = 'Drew Neavin'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
