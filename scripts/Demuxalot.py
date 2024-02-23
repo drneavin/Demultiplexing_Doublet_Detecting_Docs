5052646cc082a6075f2e63480482e99e6c7d623f
Marcel Schilling <mschilling@idibell.cat>
2024-02-23 17:03:10 +0100

    Demuxalot: Limit number of parallel jobs
    Commit e6aef103522deab5de9d14646a2b6bc1aa210152 paved the way to
    upgrade Demuxalot to the latest version (v. 0.4.0).
    However, that version includes support for parallel processing via
    `joblib.parallel`  with a default value for `n_jobs` of `-1`, resulting
    in the use of all CPUs/threads provided by the system.
    This commit adds the `-p`/`--nproc` parameter (cf. Vireo) to override
    that default.
    Since Demuxalot's default behaviour could potentially result in bad
    surprises, the default value was changed from `-1` to `1` in the wrapper
    script. Thus, existing pipelines using Demuxafy's `Demuxalot.py` script
    won't require any changes to avoid unexpected CPU loads.

--- scripts/Demuxalot.py
+++ scripts/Demuxalot.py
@@ -16,6 +16,7 @@ parser.add_argument("-n", "--indiv_file", required = True, default = None, help 
 parser.add_argument("-v", "--vcf", required = True, default = None, help = "The vcf file that has the SNP genotypes for each donor in the pool.")
 parser.add_argument("-o", "--outdir", required = True, default = None, help = "The output directory.")
 parser.add_argument("-r", "--refine", required = True, default = True, help = "Whether to run genotype refinement.")
+parser.add_argument("-p", "--nproc", required = False, type = int, default = 1, help = "(max.) number of parallel jobs; -1: number of CPU cores/threads; default: 1.")
 args = parser.parse_args()
 
 print("read arguments")
@@ -42,6 +43,7 @@ snps = count_snps(
     bamfile_location = args.bamfile,
     chromosome2positions=genotypes.get_chromosome2positions(),
     barcode_handler=barcode_handler, 
+    joblib_n_jobs=args.nproc,
 )
 
 print("estimating posterior probs and likelihoods")
