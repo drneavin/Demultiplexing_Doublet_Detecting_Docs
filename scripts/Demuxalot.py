e43de13313271bd68f9571312fb4afa82e6d6dc0
drneavin <d.neavin@garvan.org.au>
2024-05-14 14:07:09 +1000

    updated to deal with different umi and barcode flags for demuxalot

--- scripts/Demuxalot.py
+++ scripts/Demuxalot.py
@@ -3,6 +3,7 @@
 import numpy as np
 import demuxalot
 from demuxalot import Demultiplexer, BarcodeHandler, ProbabilisticGenotypes, count_snps
+from demuxalot.cellranger_specific import parse_read
 import pandas as pd
 import argparse
 import subprocess
@@ -18,6 +19,8 @@ parser.add_argument("-n", "--indiv_file", required = True, default = None, help 
 parser.add_argument("-v", "--vcf", required = True, default = None, help = "The vcf file that has the SNP genotypes for each donor in the pool.")
 parser.add_argument("-o", "--outdir", required = True, default = None, help = "The output directory.")
 parser.add_argument("-r", "--refine", required = True, default = True, help = "Whether to run genotype refinement.")
+parser.add_argument("-c", "--celltag", required = False, default = "CB", help = "optional: SAM tag used for cell barcodes; default: `CB`. ")
+parser.add_argument("-u", "--umitag", required = False, default = "UB", help = "optional: SAM tag used for UMIs; default: `UB`.")
 parser.add_argument("-p", "--nproc", required = False, type = int, default = 1, help = "(max.) number of parallel jobs; -1: number of CPU cores/threads; default: 1.")
 args = parser.parse_args()
 
@@ -46,13 +49,16 @@ genotypes.add_vcf(args.vcf)
 
 # Load barcodes
 print("loading barcodes")
-barcode_handler = BarcodeHandler.from_file(args.barcodes)
+barcode_handler = BarcodeHandler.from_file(args.barcodes, tag=args.celltag)
+
+parse_read_custom = lambda read: parse_read(read, umi_tag = args.umitag)
 
 snps = count_snps(
     bamfile_location = args.bamfile,
     chromosome2positions=genotypes.get_chromosome2positions(),
     barcode_handler=barcode_handler, 
     joblib_n_jobs=args.nproc,
+    parse_read=parse_read_custom
 )
 
 print("estimating posterior probs and likelihoods")
